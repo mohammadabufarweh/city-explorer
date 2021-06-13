8842dd6ac0188e0953ca31ae0677df6fa937d8b0
Mohammadabufarweh <abufarweh34@gmail.com>
2021-06-13 23:59:54 +0300

    lab-06

---
+++ src/Footer.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+import './Style.css';
+
+export class Footer extends Component {
+    render() {
+        return (
+            <div class='footerClass'>
+                <p>&copy; All Right Reserved</p>
+            </div>
+        )
+    }
+}
+
+export default Footer
+
---
+++ src/Header.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import './Style.css';
+export class Header extends Component {
+    render() {
+        return (
+            <div>
+                <h1 class='classHeader'>City Explor</h1>
+            </div>
+        )
+    }
+}
+
+export default Header
